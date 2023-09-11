4cf05b01042144eac2873e1aaef0f5b173167661
Evocuer <ing.cuervodiaz@gmail.com>
2023-09-11 18:43:43 -0500

    Setup del ejercicio4

---
+++ HEREJ4/src/main/java/Entidades/Circulo.java
@@ -0,0 +1,25 @@
+package Entidades;
+
+import Interfaces.calculosFormas;
+
+public class Circulo implements calculosFormas {
+
+    private Double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(Double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public void area() {
+
+    }
+
+    @Override
+    public void perimetro() {
+
+    }
+}
